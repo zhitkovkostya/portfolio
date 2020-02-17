67208686cc46cc19d7bfe73086e280bc9c6d142a
Konstantin Zhitkov <zhitkovkostya@gmail.com>
2020-02-18 01:12:34 +0300

    add sanity studio

---
+++ sanity/schemas/blockContent.js
@@ -0,0 +1,40 @@
+/**
+ * This is the schema definition for the rich text fields used for
+ * for this blog studio. When you import it in schemas.js it can be
+ * reused in other parts of the studio with:
+ *  {
+ *    name: 'someName',
+ *    title: 'Some title',
+ *    type: 'blockContent'
+ *  }
+ */
+export default {
+  title: 'Block Content',
+  name: 'blockContent',
+  type: 'array',
+  of: [
+    {
+      title: 'Block',
+      type: 'block',
+      styles: [],
+      lists: [],
+      marks: {
+        decorators: [],
+        annotations: [
+          {
+            title: 'URL',
+            name: 'link',
+            type: 'object',
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}
